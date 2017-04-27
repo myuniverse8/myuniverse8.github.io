356d5bcb15c98e97f3e7bc40b3191e3c06b8fa17
myuniverse8 <myuniverse8888@gmail.com>
2017-04-27 10:44:48 +0200

    Fix some issues

--- js/myscript.js
+++ js/myscript.js
@@ -1348,7 +1348,6 @@ sortObjList = function(option) {
 }
 
 function scrollFunction() {
-    debugger;
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
     } else {
